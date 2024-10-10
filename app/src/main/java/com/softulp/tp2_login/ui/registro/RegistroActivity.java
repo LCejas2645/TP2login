ef783f4fb4d8b7c8264a4dfb7a4117ad81b4f5fd
LCejas2645 <101837605+LCejas2645@users.noreply.github.com>
2024-10-09 22:54:57 -0300

    funciona

--- app/src/main/java/com/softulp/tp2_login/ui/registro/RegistroActivity.java
+++ app/src/main/java/com/softulp/tp2_login/ui/registro/RegistroActivity.java
@@ -36,10 +36,6 @@ import com.softulp.tp2_login.ui.login.MainActivity;
 
 
 public class RegistroActivity extends AppCompatActivity {
-    private ImageView imagen1;
-    private EditText et1;
-    //private MainViewModel vm;
-    private static int REQUEST_IMAGE_CAPTURE=1;
     ActivityRegistroBinding binding;
     RegistroActivityViewModel mv;
 
@@ -61,127 +57,25 @@ public class RegistroActivity extends AppCompatActivity {
                 String mail = binding.etMail.getText().toString();
                 String password = binding.etPassword2.getText().toString();
 
-                Usuario usuario = new Usuario(dni,nombre,apellido,mail,password);
-                Api.guardarUsuario(getApplication(),usuario);
+                Usuario usuario = new Usuario(dni, nombre, apellido, mail, password);
+                Api.guardarUsuario(getApplication(), usuario);
             }
         });
-
 
 
         mv.getUsuarioMutable().observe(this, new Observer<Usuario>() {
             @Override
             public void onChanged(Usuario usuario) {
-                binding.etDni.setText(usuario.getDni()+"");
-                binding.etNombre.setText(usuario.getNombre()+"");
-                binding.etApellido.setText(usuario.getApellido()+"");
-                binding.etMail.setText(usuario.getMail()+"");
-                binding.etPassword2.setText(usuario.getPassword()+"");
+                binding.etDni.setText(usuario.getDni() + "");
+                binding.etNombre.setText(usuario.getNombre() + "");
+                binding.etApellido.setText(usuario.getApellido() + "");
+                binding.etMail.setText(usuario.getMail() + "");
+                binding.etPassword2.setText(usuario.getPassword() + "");
             }
         });
 
         Intent intent = getIntent();
         Usuario usuario = (Usuario) intent.getSerializableExtra("usuario");
         mv.recuperarUsuario(usuario);
-        configView();
-    }
-
-    public void configView(){
-        imagen1=binding.ivFoto;
-        et1=binding.etFoto;
-        validaPermisos();
-        mv= ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication()).create(RegistroActivityViewModel.class);
-        mv.getFoto().observe(this, new Observer<Bitmap>() {
-            @Override
-            public void onChanged(Bitmap bitmap) {
-                imagen1.setImageBitmap(bitmap);
-            }
-        });
-    }
-    public void tomarFoto(View v){
-//startActivityForResult es otra forma de iniciar una activity, pero esperando desde donde la llamé un resultado
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
-    }
-
-    //Este método es llamado automáticamente cuando retorna de la cámara.
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        Log.d("salida", "onActivityResult: "+data);
-        mv.respuetaDeCamara(requestCode,resultCode,data,REQUEST_IMAGE_CAPTURE);
-    }
-
-
-
-    private boolean validaPermisos() {
-
-        if(Build.VERSION.SDK_INT<Build.VERSION_CODES.M){
-            return true;
-        }
-
-        if((checkSelfPermission(CAMERA)== PackageManager.PERMISSION_GRANTED)){
-            return true;
-        }
-
-        if((shouldShowRequestPermissionRationale(CAMERA))){
-            cargarDialogoRecomendacion();
-        }else{
-            requestPermissions(new String[]{CAMERA},100);
-        }
-
-        return false;
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if(requestCode==100){
-            if(grantResults.length==2 && grantResults[0]==PackageManager.PERMISSION_GRANTED
-                    && grantResults[1]==PackageManager.PERMISSION_GRANTED){
-
-            }else{
-                solicitarPermisosManual();
-            }
-        }
-
-    }
-
-    private void solicitarPermisosManual() {
-        final CharSequence[] opciones={"si","no"};
-        final AlertDialog.Builder alertOpciones=new AlertDialog.Builder(RegistroActivity.this);
-        alertOpciones.setTitle("¿Desea configurar los permisos de forma manual?");
-        alertOpciones.setItems(opciones, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                if (opciones[i].equals("si")){
-                    Intent intent=new Intent();
-                    intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-                    Uri uri=Uri.fromParts("package",getPackageName(),null);
-                    intent.setData(uri);
-                    startActivity(intent);
-                }else{
-                    Toast.makeText(getApplicationContext(),"Los permisos no fueron aceptados",Toast.LENGTH_SHORT).show();
-                    dialogInterface.dismiss();
-                }
-            }
-        });
-        alertOpciones.show();
-    }
-
-    private void cargarDialogoRecomendacion() {
-        AlertDialog.Builder dialogo=new AlertDialog.Builder(RegistroActivity.this);
-        dialogo.setTitle("Permisos Desactivados");
-        dialogo.setMessage("Debe aceptar los permisos para el correcto funcionamiento de la App");
-
-        dialogo.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                requestPermissions(new String[]{CAMERA},100);
-            }
-        });
-        dialogo.show();
     }
 }
--- app/src/main/java/com/softulp/tp2_login/ui/registro/RegistroActivityViewModel.java
+++ app/src/main/java/com/softulp/tp2_login/ui/registro/RegistroActivityViewModel.java
@@ -26,7 +26,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class RegistroActivityViewModel extends AndroidViewModel {
-    private MutableLiveData<Bitmap> foto;
     private MutableLiveData <Usuario> usuarioMutable;
 
     private Context context;
@@ -41,59 +40,12 @@ public class RegistroActivityViewModel extends AndroidViewModel {
         }
         return usuarioMutable;
     }
-    public LiveData<Bitmap> getFoto(){
-        if(foto==null){
-            foto=new MutableLiveData<>();
-        }
-        return foto;
-    }
+
 
     public void recuperarUsuario(Usuario usuario){
 
         if(usuario != null) {
             usuarioMutable.setValue(usuario);
-        }
-    }
-    public void respuetaDeCamara(int requestCode, int resultCode, @Nullable Intent data, int REQUEST_IMAGE_CAPTURE){
-        Log.d("salida",requestCode+"");
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            //Recupero los datos provenientes de la camara.
-            Bundle extras = data.getExtras();
-            Log.d("salida", "extras: "+extras);
-            //Casteo a bitmap lo obtenido de la camara.
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-
-            //Rutina para optimizar la foto,
-            ByteArrayOutputStream baos=new ByteArrayOutputStream();
-            imageBitmap.compress(Bitmap.CompressFormat.PNG,100, baos);
-            Log.d("salida", "imageBitmap: "+imageBitmap);
-            foto.setValue(imageBitmap);
-
-
-
-            //Rutina para convertir a un arreglo de byte los datos de la imagen
-            byte [] b=baos.toByteArray();
-
-
-            //Aquí podría ir la rutina para llamar al servicio que recibe los bytes.
-            File archivo =new File(context.getFilesDir(),"foto1.png");
-            if(archivo.exists()){
-                archivo.delete();
-            }
-            try {
-                FileOutputStream fo=new FileOutputStream(archivo);
-                BufferedOutputStream bo=new BufferedOutputStream(fo);
-                bo.write(b);
-                bo.flush();
-                bo.close();
-//                Intent segunda=new Intent(context,RegistroActivity.class);
-//                segunda.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//                context.startActivity(segunda);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
